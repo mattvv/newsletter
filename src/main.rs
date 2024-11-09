f903acf6d491d46fb3613cc2fdafe6025a09fcb3
Matt Van <mattvan@proofofplay.gg>
2024-11-09 11:23:59 -0400

    save to db and test

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::net::TcpListener;
 
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,15 +2,20 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 
 use crate::routes::health_check;
 use crate::routes::subscribe;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
+    //wrap connection in smart pointer
+    let connection = web::Data::new(connection_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
