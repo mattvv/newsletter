b0ea8fe3dd890cb9d2296263005b84b119ad0fb4
Matt Van <mattvan@proofofplay.gg>
2024-11-11 04:39:35 -0400

    telemetry

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use std::net::TcpListener;
 
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use newsletter::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let susbcriber = get_subscriber("newsletter".into(), "info".into());
+    init_subscriber(susbcriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -2,13 +2,10 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
     //wrap connection in smart pointer
     let connection = web::Data::new(connection_pool);
 
