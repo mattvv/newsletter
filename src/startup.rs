2a8841e0b67db47c472acecf413fce942114f4ae
Matt Van <mattvan@proofofplay.gg>
2024-11-11 03:44:42 -0400

    basic logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,20 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
+use env_logger::Env;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
-
-use crate::routes::health_check;
-use crate::routes::subscribe;
-
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     //wrap connection in smart pointer
     let connection = web::Data::new(connection_pool);
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
